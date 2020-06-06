d77f9a3527d032c77eb569dc1aad7135cc5f5ff4
Ryan Flynn <ryanflynn44@gmail.com>
2020-06-06 16:26:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,4 +14,11 @@ def using_flatten (array)
   array.flatten 
 end  
 
-def 
+def using_delete (array, string)
+  array.delete(string)
+end 
+
+def using_delete_at (array, n)
+  array.delete_at(n)
+end
+  
